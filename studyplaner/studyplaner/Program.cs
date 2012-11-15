41a669803349e471de8ed5b6803aed1b3c13d32c
Florian Wilkens <floya@live.de>
2012-11-15 17:10:24 +0100

    test

--- studyplaner/studyplaner/Program.cs
+++ studyplaner/studyplaner/Program.cs
@@ -15,7 +15,8 @@ namespace studyplaner
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            System.Console.WriteLine("U Suck");
         }
     }
 }
