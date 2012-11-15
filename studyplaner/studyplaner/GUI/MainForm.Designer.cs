e16744cca69bd9f4aff8ae5191794ae72f6eff7f
Dave J <contact_davej@ymail.com>
2012-11-15 23:50:15 +0100

    edited GUI

--- studyplaner/studyplaner/GUI/MainForm.Designer.cs
+++ studyplaner/studyplaner/GUI/MainForm.Designer.cs
@@ -28,7 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this._mainPanel = new System.Windows.Forms.Panel();
+            this._mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this._panelInfo = new System.Windows.Forms.Panel();
+            this._mainMenu_file_new = new System.Windows.Forms.ToolStripMenuItem();
+            this._mainMenu_file_quit = new System.Windows.Forms.ToolStripMenuItem();
+            this._mainMenuStrip.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // _mainPanel
+            // 
+            this._mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._mainPanel.BackColor = System.Drawing.SystemColors.Control;
+            this._mainPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._mainPanel.Location = new System.Drawing.Point(12, 37);
+            this._mainPanel.Name = "_mainPanel";
+            this._mainPanel.Size = new System.Drawing.Size(685, 513);
+            this._mainPanel.TabIndex = 0;
+            // 
+            // _mainMenuStrip
+            // 
+            this._mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this._mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this._mainMenuStrip.Name = "_mainMenuStrip";
+            this._mainMenuStrip.Size = new System.Drawing.Size(984, 24);
+            this._mainMenuStrip.TabIndex = 1;
+            this._mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this._mainMenu_file_new,
+            this._mainMenu_file_quit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // _panelInfo
+            // 
+            this._panelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._panelInfo.BackColor = System.Drawing.SystemColors.Control;
+            this._panelInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._panelInfo.Location = new System.Drawing.Point(703, 37);
+            this._panelInfo.Name = "_panelInfo";
+            this._panelInfo.Size = new System.Drawing.Size(269, 513);
+            this._panelInfo.TabIndex = 2;
+            // 
+            // _mainMenu_file_new
+            // 
+            this._mainMenu_file_new.Name = "_mainMenu_file_new";
+            this._mainMenu_file_new.Size = new System.Drawing.Size(152, 22);
+            this._mainMenu_file_new.Text = "New";
+            // 
+            // _mainMenu_file_quit
+            // 
+            this._mainMenu_file_quit.Name = "_mainMenu_file_quit";
+            this._mainMenu_file_quit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this._mainMenu_file_quit.Size = new System.Drawing.Size(152, 22);
+            this._mainMenu_file_quit.Text = "Quit";
             // 
             // MainForm
             // 
@@ -36,13 +115,29 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightSlateGray;
             this.ClientSize = new System.Drawing.Size(984, 562);
+            this.Controls.Add(this._panelInfo);
+            this.Controls.Add(this._mainPanel);
+            this.Controls.Add(this._mainMenuStrip);
+            this.MainMenuStrip = this._mainMenuStrip;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "StudyPlaner";
+            this._mainMenuStrip.ResumeLayout(false);
+            this._mainMenuStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel _mainPanel;
+        private System.Windows.Forms.MenuStrip _mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Panel _panelInfo;
+        private System.Windows.Forms.ToolStripMenuItem _mainMenu_file_new;
+        public System.Windows.Forms.ToolStripMenuItem _mainMenu_file_quit;
     }
 }
