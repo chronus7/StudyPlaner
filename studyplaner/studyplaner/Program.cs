bd326b2a22fe09063e62aa03eaddb50b9bf54374
Dave <github.com/DaveAtGit>
2012-11-15 17:19:08 +0100

    standard Form1 deleted...

--- studyplaner/studyplaner/Program.cs
+++ studyplaner/studyplaner/Program.cs
@@ -15,7 +15,7 @@ namespace studyplaner
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
         }
     }
 }
