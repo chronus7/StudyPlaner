9a551a894b814080b7c129844e110e0f4efa72c4
Florian Wilkens <floya@live.de>
2012-11-15 17:49:17 +0100

    Test git extensions

--- studyplaner/studyplaner/Program.cs
+++ studyplaner/studyplaner/Program.cs
@@ -15,7 +15,6 @@ namespace studyplaner
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new Form1());
         }
     }
 }
