4671adf00ce407db2f84f105d731bb54108bb23e
Dave J <contact_davej@ymail.com>
2012-11-17 17:40:15 +0100

    Rise of the plans...

---
+++ studyplaner/studyplaner/Materials/Plan.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Studyplaner.Materials
+{
+    class Plan
+    {
+        public String Name { get; set; }
+        public List<UniEvent> Events { get; set; }
+    }
+}
