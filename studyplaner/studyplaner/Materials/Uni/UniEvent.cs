19230b9b3f039752aab0a09b9f05ed5df8b09bba
Dave J <contact_davej@ymail.com>
2012-11-23 07:25:52 +0100

    reworked property. Added comments.

--- studyplaner/studyplaner/Materials/Uni/UniEvent.cs
+++ studyplaner/studyplaner/Materials/Uni/UniEvent.cs
@@ -9,33 +9,77 @@ namespace Studyplaner.Materials.Uni
     [XmlRootAttribute("event")]
     public class UniEvent
     {
+        /// <summary>
+        /// The (hopefully) unique ID of the event.
+        /// </summary>
         [XmlElement(ElementName="id")]
         public int ID { get; set; }
+        
+        /// <summary>
+        /// The (hopefully) unique ID of the event's
+        /// parent module.
+        /// </summary>
         [XmlIgnore]
         public int ModuleID { get; set; }
+        
+        /// <summary>
+        /// The LV-Number of the event.
+        /// </summary>
         [XmlElement(ElementName = "lvnum")]
         public string LVNum { get; set; }
+        
+        /// <summary>
+        /// The docation, where the event takes place.
+        /// </summary>
         [XmlElement(ElementName = "location")]
         public string Location { get; set; }
+        
+        /// <summary>
+        /// The time/date, when the event starts.
+        /// </summary>
         [XmlElement(ElementName = "date")]
         public Time Date { get; set; }
+        
+        /// <summary>
+        /// The duration of the event.
+        /// </summary>
         [XmlIgnore]
         public TimeSpan Duration { get; set; }
-        [XmlElement(ElementName = "lecturer")]
-        public string Lecturer { get; set; }
-        [XmlElement(ElementName = "importance")]
-        public byte Importance { get; set; }
-        [XmlElement(ElementName = "power")]
-        public bool Power { get; set; }
-        [XmlElement(ElementName = "type")]
-        public EventType Type { get; set; }
-
+        /// <summary>
+        /// DO NOT USE THIS. IT JUST FOR SERIALIZATION!
+        /// </summary>
         [Browsable(false), EditorBrowsable(EditorBrowsableState.Never)] // <-- seems not to work
         [XmlElement(ElementName = "duration")]
-        public long DurationTicks
+        public string DurationSerialize
         {
-            get { return Duration.Ticks; }
-            set { Duration = new TimeSpan(value); }
+            get { return Duration.ToString(); }
+            set { Duration = TimeSpan.Parse(value); }
         }
+        
+        /// <summary>
+        /// The persons, standing at the front.
+        /// </summary>
+        [XmlElement(ElementName = "lecturer")]
+        public string Lecturer { get; set; }
+        
+        /// <summary>
+        /// The importance of this event.
+        /// Mainly for notifications.
+        /// </summary>
+        [XmlElement(ElementName = "importance")]
+        public byte Importance { get; set; }
+        
+        /// <summary>
+        /// Wether the location, the event takes place,
+        /// has a reachable power-plug or not.
+        /// </summary>
+        [XmlElement(ElementName = "power")]
+        public bool Power { get; set; }
+
+        /// <summary>
+        /// The type of this event.
+        /// </summary>
+        [XmlElement(ElementName = "type")]
+        public EventType Type { get; set; }
     }
 }
