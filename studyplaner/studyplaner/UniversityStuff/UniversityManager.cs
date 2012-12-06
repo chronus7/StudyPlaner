ce9d2fb8ad62dd5d2648f22ba2e33c4810d6c317
Florian Wilkens <floya@live.de>
2012-12-06 23:59:16 +0100

    made UniversityManager static and added mostly dummy methods..
    the valid id checks should work tho.. ;)

--- studyplaner/studyplaner/UniversityStuff/UniversityManager.cs
+++ studyplaner/studyplaner/UniversityStuff/UniversityManager.cs
@@ -3,32 +3,55 @@ using System.Collections.Generic;
 
 namespace Studyplaner.UniversityStuff
 {
-    public class UniversityManager
+    public static class UniversityManager
     {
-        private HashSet<ushort> _usedIDs;
-        private Dictionary<ushort, University> _universities;
+        private const ulong MAXSIZE_UNIVERSITY  = 10000;
+        private const ulong MAXSIZE_MODULE = 1000000000;
+        private const ulong MAXSIZE_EVENT = 100000000000;
 
-        public UniversityManager(string uniDirectory)
+        private static Dictionary<ushort, University> _universities;
+        private static Dictionary<uint, UniversityModule> _modules;
+        private static Dictionary<ulong, UniversityEvent> _events;
+
+        private static bool _initialized = false;
+
+        /// <summary>
+        /// Initializes the UniversityManager and loads all previously persisted data from xmls
+        /// </summary>
+        public static void Initialize()
         {
-            _usedIDs = new HashSet<ushort>();
+            _universities = new Dictionary<ushort, University>();
+            _modules = new Dictionary<uint, UniversityModule>();
+            _events = new Dictionary<ulong, UniversityEvent>();
 
-            _universities = LoadUniversities(uniDirectory);
+            // TODO: deserialize values with path from settings
+
+            _initialized = true;
+        }
+
+        /// <summary>
+        /// Returns a value wether the UniversityManager has been correctly initialized
+        /// </summary>
+        /// <returns>Has the UniversityManager been correctly initialized?</returns>
+        public static bool IsInitialized()
+        {
+            return _initialized;
         }
 
         /// <summary>
         /// Generates the next valid ID for a new University
         /// </summary>
-        /// <returns>The next ID if there is one, else -1</returns>
-        private ushort GenerateNextID()
+        /// <returns>The next ID if there is one, else -1</returns> //TODO: change to fit
+        private static ushort GenerateNextID()
         {
             ushort s = 1;
-            while (_usedIDs.Contains(s))
-            {
-                if (s < ushort.MaxValue)
-                    s++;
-                else
-                    return 0;
-            }
+            //while (_usedIDs.Contains(s))
+            //{
+            //    if (s < ushort.MaxValue)
+            //        s++;
+            //    else
+            //        return 0;
+            //}
             return s;
         }
 
@@ -39,16 +62,63 @@ namespace Studyplaner.UniversityStuff
         /// <returns>A value representing the success of the operation.
         ///     - true, if the id was added
         ///     - false oherwise</returns>
-        private bool RegisterNewID(ushort id)
+        private static bool RegisterNewID(ushort id)       // might want to put that together with adding
         {
-            if (_usedIDs.Contains(id))
-                return false;
+            //if (_usedIDs.Contains(id))
+            //    return false;
 
-            _usedIDs.Add(id);
-            return true;
+            //_usedIDs.Add(id);
+            //return true;
+            return false;
         }
 
-        private Dictionary<ushort, University> LoadUniversities(string uniDirectory)
+        private static bool IsValidID(ulong id)
+        {
+            if (id > MAXSIZE_EVENT)
+                return false;
+
+            if (id < MAXSIZE_UNIVERSITY)
+                return ContainsUniversity((ushort)id);
+            if (id < MAXSIZE_MODULE)
+                return ContainsModule((uint)id);
+
+            return ContainsEvent(id);
+        }
+
+        private static bool ContainsUniversity(ushort id)
+        {
+            foreach (ushort key in _universities.Keys)
+            {
+                if (id == key)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static bool ContainsModule(uint id)
+        {
+            foreach (uint key in _modules.Keys)
+            {
+                if (id == key)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static bool ContainsEvent(ulong id)
+        {
+            foreach (ulong key in _events.Keys)
+            {
+                if (id == key)
+                    return true;
+            }
+
+            return false;
+        }
+
+        private static Dictionary<ushort, University> LoadUniversities(string uniDirectory)
         {
             //TODO: |f| load universities from given path.. remember to add values to +_usedIDs;
             //by the way... do we really wanna it this way??
@@ -61,24 +131,24 @@ namespace Studyplaner.UniversityStuff
         /// Get called at constrution time to build the initial University-List
         /// </summary>
         /// <param name="toAdd">Die Universität, die hinzugefügt werden soll</param>
-        public void AddUniversity(University toAdd)         //TODO: might want to return the id of the added uni?
+        public static void AddUniversity(University toAdd)         //TODO: might want to return the id of the added uni?
         {
             if (toAdd == null)
                 throw new ArgumentNullException("toAdd");
 
         }
 
-        public void RemoveUniversity(ushort id)
+        public static void RemoveUniversity(ushort id)
         {
             
         }
 
-        public void AddModule(ushort uniID, UniversityModule module)
+        public static void AddModule(ushort uniID, UniversityModule module)
         {
             
         }
 
-        public void RemoveModule(ushort uniID, uint moduleID)
+        public static void RemoveModule(ushort uniID, uint moduleID)
         {
 
         }
