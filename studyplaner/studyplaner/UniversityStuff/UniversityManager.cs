11e91e113b5dd3a3aabba036befeec3a1727aeed
Florian Wilkens <floya@live.de>
2012-12-09 00:29:44 +0100

    not done yet.. but im tired zz

--- studyplaner/studyplaner/UniversityStuff/UniversityManager.cs
+++ studyplaner/studyplaner/UniversityStuff/UniversityManager.cs
@@ -55,64 +55,92 @@ namespace Studyplaner.UniversityStuff
         }
 
         /// <summary>
-        /// Adds a University o the manager
+        /// Adds a University o the UniversityManager
         /// Gets called multiple times at constrution time to build the initial University-List
         /// </summary>
         /// <param name="toAdd">The University to add</param>
         /// <returns>The ID that was used to add the University</returns>
-        public static ushort AddUniversity(University toAdd)         //TODO: might want to return the id of the added uni?
+        public static ushort AddUniversity(University toAdd)
         {
             CheckInitialization();
 
             if (toAdd == null)
                 throw new ArgumentNullException("toAdd");
 
-            ushort newID = (ushort)toAdd.ID; //TODO: need to change the id fields in the classes to the proper type
-            if (ContainsUniversity(newID))
+            ushort newID = (ushort)toAdd.ID;                        //TODO: need to change the id fields in the classes to the proper type
+            if (!IsValidID(newID) || ContainsUniversity(newID))
                 newID = GenerateNewID();
+
+            toAdd.ID = newID;
+            _universities.Add(newID, toAdd);
 
             return (ushort)toAdd.ID;
         }
 
-        public static University GetUniversity(ushort id)
+        /// <summary>
+        /// Gets the University with the given ID from the UniversityManager
+        /// </summary>
+        /// <param name="uniID">The ID of the University to get</param>
+        /// <returns>The University if it is in the manager, null if not</returns>
+        public static University GetUniversity(ushort uniID)
         {
             CheckInitialization();
 
-            if (ContainsUniversity(id))
-                return _universities[id];
+            if (ContainsUniversity(uniID))
+                return _universities[uniID];
 
             return null;                    //TODO: return null or throw an exception?
         }
 
-        public static void RemoveUniversity(ushort id)
+        /// <summary>
+        /// Removes a University from the UniversityManager
+        /// </summary>
+        /// <param name="uniID">The ID of the University to remove</param>
+        public static void RemoveUniversity(ushort uniID)
         {
             CheckInitialization();
 
-            _universities.Remove(id);       //TODO: can be called even if the key doesnt exist.. do we want to do nothing or throw an exception?
+            _universities.Remove(uniID);       //TODO: can be called even if the key doesnt exist.. do we want to do nothing or throw an exception?
         }
 
+        /// <summary>
+        /// Adds a Module to the UniversityManager
+        /// </summary>
+        /// <param name="uniID">The ID of the University to which the UniversityModule belongs</param>
+        /// <param name="toAdd">The Module to add</param>
+        /// <returns>The ID that was used to add the University</returns>
         public static uint AddModule(ushort uniID, UniversityModule toAdd)
         {
             CheckInitialization();
 
-            CheckInitialization();
-
-            if (toAdd == null)
-                throw new ArgumentNullException("toAdd");
-
-            uint newID = (uint)toAdd.ID; //TODO: need to change the id fields in the classes to the proper type
-            if (ContainsModule(newID))
-                newID = GenerateNewID();
-
-            return (uint)toAdd.ID;
+            return 0;
         }
 
-        public static void RemoveModule(ushort uniID, uint moduleID)
+        /// <summary>
+        /// Gets a Module with the given ID from the UniversityManager
+        /// </summary>
+        /// <param name="moduleID">The ID of the UniversityModule to get</param>
+        /// <returns>The UniversityModule if it is in the Manager. null if not</returns>
+        public static UniversityModule GetModule(uint moduleID)
         {
             CheckInitialization();
+
+            return null;            //TODO: haha
         }
 
-        public static bool IsValidID(ulong id)
+        /// <summary>
+        /// Removes a Module from the UniversityManager
+        /// </summary>
+        /// <param name="uniID"></param>
+        /// <param name="moduleID"></param>
+        public static void RemoveModule(uint moduleID)
+        {
+            CheckInitialization();
+
+            //TODO: REmove the module
+        }
+
+        private static bool IsValidID(ulong id)
         {
             ushort uni = (ushort)(id / (MULTIPLYER_UNIVERSITY * MULTIPLYER_MODULE));
             uint module = (uint)(id % (MULTIPLYER_UNIVERSITY * MULTIPLYER_MODULE) / MULTIPLYER_MODULE);
@@ -121,7 +149,7 @@ namespace Studyplaner.UniversityStuff
             return (IsValidID(uni) && IsValidID(module) && (evnt > 0) && (evnt < MULTIPLYER_MODULE));
         }
 
-        public static bool IsValidID(uint id)
+        private static bool IsValidID(uint id)
         {
             ushort uni = (ushort)(id / MULTIPLYER_UNIVERSITY);
             uint module = id % MULTIPLYER_UNIVERSITY;
@@ -129,11 +157,16 @@ namespace Studyplaner.UniversityStuff
             return (IsValidID(uni) && (module > 0 && module < MULTIPLYER_UNIVERSITY));
         }
 
-        public static bool IsValidID(ushort id)
+        private static bool IsValidID(ushort id)
         {
             return (id > 0) && (id < MAXSIZE_UNIVERSITY);
         }
 
+        /// <summary>
+        /// Returns wether the UniversityManager contains an Item with the given ID
+        /// </summary>
+        /// <param name="id">The Id to check</param>
+        /// <returns>Teh result</returns>
         public static bool ContainsID(ulong id)
         {
             CheckInitialization();
@@ -155,7 +188,7 @@ namespace Studyplaner.UniversityStuff
             ushort id = 1;
             while (ContainsUniversity(id))
             {
-                if (id < ushort.MaxValue)
+                if (id < MAXSIZE_UNIVERSITY)
                     id++;
                 else
                     throw new OverflowException(ERROR_ID_OVERFLOW);
@@ -166,10 +199,10 @@ namespace Studyplaner.UniversityStuff
 
         private static uint GenerateNewID(ushort universityID)
         {
-            uint id = universityID * MULTIPLYER_UNIVERSITY;
+            uint id = universityID * MULTIPLYER_UNIVERSITY + 1;
             while (ContainsModule(id))
             {
-                if (id < uint.MaxValue)
+                if (id < MAXSIZE_MODULE)
                     id++;
                 else
                     throw new OverflowException(ERROR_ID_OVERFLOW);
@@ -180,7 +213,16 @@ namespace Studyplaner.UniversityStuff
 
         private static ulong GenerateNewID(uint moduleID)
         {
-            return 0;
+            ulong id = moduleID * MULTIPLYER_MODULE + 1;
+            while (ContainsEvent(id))
+            {
+                if (id < MAXSIZE_EVENT)
+                    id++;
+                else
+                    throw new OverflowException(ERROR_ID_OVERFLOW);
+            }
+
+            return id;
         }
 
         private static bool ContainsUniversity(ushort id)
